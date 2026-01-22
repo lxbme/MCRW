32564dfe1881b17c3cfdc680f26ab9757afcb26e
lxbme <18108274905@163.com>
2026-01-22 16:05:55 +0800

    apply cargo fmt

--- src/lua_ctx.rs
+++ src/lua_ctx.rs
@@ -1,11 +1,31 @@
-use std::{fs, path::Path, sync::{Arc, Mutex}};
+// MCRW is a extendable management framework for minecraft
+// Copyright (C) 2026  YUHAN LI
 
-use regex::Regex;
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::{
+    fs,
+    path::Path,
+    sync::{Arc, Mutex},
+};
+
 use mlua::{Function, Lua, RegistryKey, Table, UserData, UserDataMethods, Value};
+use regex::Regex;
 
 pub struct Trigger {
     pub regex: Regex,
-    pub callback: RegistryKey
+    pub callback: RegistryKey,
 }
 
 // list of lua plugins callback
@@ -26,33 +46,40 @@ pub struct WrapperApi;
 
 impl UserData for WrapperApi {
     fn add_methods<M: UserDataMethods<Self>>(methods: &mut M) {
-        methods.add_method("register", |lua: &Lua, _this: &WrapperApi, (pattern, func): (String, Function)| {
-            let regex = Regex::new(&pattern).map_err(|e| mlua::Error::external(e))?;
-            let callback = lua.create_registry_value(func)?;
-            let triggers_check: mlua::AnyUserData = lua.globals().get("__internal_triggers")?;
-            let trigger_list: TriggerListWrapper = triggers_check.borrow::<TriggerListWrapper>()?.clone();
-            trigger_list.0.lock().unwrap().push(Trigger { regex, callback });
-            
-            Ok(())
-        });
+        methods.add_method(
+            "register",
+            |lua: &Lua, _this: &WrapperApi, (pattern, func): (String, Function)| {
+                let regex = Regex::new(&pattern).map_err(|e| mlua::Error::external(e))?;
+                let callback = lua.create_registry_value(func)?;
+                let triggers_check: mlua::AnyUserData = lua.globals().get("__internal_triggers")?;
+                let trigger_list: TriggerListWrapper =
+                    triggers_check.borrow::<TriggerListWrapper>()?.clone();
+                trigger_list
+                    .0
+                    .lock()
+                    .unwrap()
+                    .push(Trigger { regex, callback });
+
+                Ok(())
+            },
+        );
     }
 }
 
 pub fn load_plugins(lua: &Lua) -> mlua::Result<()> {
     let plugins_dir = Path::new("lua_plugins");
-    
-    
+
     let globals = lua.globals();
     let package: Table = globals.get("package")?;
     let current_path: String = package.get("path")?;
-    
+
     let new_path = format!("lua_plugins/?.lua;lua_plugins/?/init.lua;{}", current_path);
     package.set("path", new_path)?;
 
     for entry in fs::read_dir(plugins_dir)? {
         let entry = entry?;
         let path = entry.path();
-        
+
         if path.is_dir() {
             let plugin_name = path.file_name().unwrap().to_str().unwrap();
             let init_path = path.join("init.lua");
@@ -61,7 +88,7 @@ pub fn load_plugins(lua: &Lua) -> mlua::Result<()> {
                 println!("[MCRW] Loading plugin module: lua_plugins.{}", plugin_name);
                 let require: Function = globals.get("require")?;
                 let module_name = format!("lua_plugins.{}.", plugin_name);
-                
+
                 if let Err(e) = require.call::<Value>(module_name) {
                     eprintln!("[Error] Failed to load plugin {}: {}", plugin_name, e);
                 }
--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,17 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-mod utils;
 mod lua_ctx;
+mod utils;
 
+use lua_ctx::{TriggerList, TriggerListWrapper, WrapperApi};
+use mlua::{Function, Lua, Value};
 use std::env;
 use std::process::Stdio;
+use std::sync::{Arc, Mutex};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::process::Command;
 use tokio::sync::mpsc;
-use mlua::{Function, Lua, Value};
-use std::sync::{Arc, Mutex};
-use lua_ctx::{TriggerList, WrapperApi, TriggerListWrapper};
 
 #[tokio::main]
 async fn main() {
@@ -42,19 +42,30 @@ async fn main() {
     let lua = Lua::new();
     let triggers: TriggerList = Arc::new(Mutex::new(Vec::new()));
     let triggers_wrapper = TriggerListWrapper(triggers.clone());
-    lua.globals().set("__internal_triggers", triggers_wrapper).expect("[MCRW] [PANIC] Fail to attach trigger list to lua");
-    lua.globals().set("wrapper", WrapperApi).expect("[MCRW] [PANIC] Fail to attach wrapper to lua");
+    lua.globals()
+        .set("__internal_triggers", triggers_wrapper)
+        .expect("[MCRW] [PANIC] Fail to attach trigger list to lua");
+    lua.globals()
+        .set("wrapper", WrapperApi)
+        .expect("[MCRW] [PANIC] Fail to attach wrapper to lua");
 
     lua_ctx::load_plugins(&lua).expect("[MCRW] [PANIC] Fail to load plugins");
-    println!("[MCRW] Lua script loaded. Registered {} triggers.", triggers.lock().unwrap().len());
+    println!(
+        "[MCRW] Lua script loaded. Registered {} triggers.",
+        triggers.lock().unwrap().len()
+    );
 
-    println!("[MCRW] Starting server with args: {}", server_args.join(" "));
+    println!(
+        "[MCRW] Starting server with args: {}",
+        server_args.join(" ")
+    );
     let mut child = Command::new("java")
         // .args(&["-Xmx1024M", "-Xms1024M", "-jar", server_path, "nogui"])
         .args(&server_args[1..])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
-        .spawn().expect("[MCRW] [PANIC] Fail to minecraft server");
+        .spawn()
+        .expect("[MCRW] [PANIC] Fail to minecraft server");
     println!("[MCRW] Server Started.");
 
     let stdout = child.stdout.take().expect("Failed to open stdout");
@@ -66,7 +77,11 @@ async fn main() {
     // Command consumer
     tokio::spawn(async move {
         while let Some(cmd) = rx.recv().await {
-            let cmd_with_newline: String = if cmd.ends_with('\n') { cmd } else { format!("{}\n", cmd) };
+            let cmd_with_newline: String = if cmd.ends_with('\n') {
+                cmd
+            } else {
+                format!("{}\n", cmd)
+            };
             if let Err(e) = stdin.write_all(cmd_with_newline.as_bytes()).await {
                 eprintln!("[Error] Failed to write to server stdin: {}", e);
                 break;
@@ -83,7 +98,7 @@ async fn main() {
     tokio::spawn(async move {
         let mut reader = BufReader::new(tokio::io::stdin());
         let mut line = String::new();
-        
+
         while reader.read_line(&mut line).await.unwrap_or(0) > 0 {
             // send cmd to channel
             if tx_for_terminal.send(line.clone()).await.is_err() {
@@ -92,7 +107,7 @@ async fn main() {
             line.clear();
         }
     });
-    
+
     // main loop producer
     let tx_main = tx.clone();
     while let Some(line) = reader.next_line().await.expect("Fail to read line") {
@@ -102,7 +117,8 @@ async fn main() {
 
         // trigger gard field
         {
-            let triggers_gard = triggers.lock()
+            let triggers_gard = triggers
+                .lock()
                 .expect("[MCRW] [PANIC] Fail to lock trigger list");
             for trigger in triggers_gard.iter() {
                 if let Some(caps) = trigger.regex.captures(&line) {
@@ -111,15 +127,17 @@ async fn main() {
                     args.push(Value::String(lua.create_string(&line).unwrap())); // full line for first
 
                     for i in 1..caps.len() {
-                        let cap_str: &str = caps.get(i).map_or("", |m: regex::Match<'_>| m.as_str());
+                        let cap_str: &str =
+                            caps.get(i).map_or("", |m: regex::Match<'_>| m.as_str());
                         args.push(Value::String(lua.create_string(cap_str).unwrap()));
                     }
 
                     // fetch call back and execute
                     let func: Function = lua.registry_value(&trigger.callback).unwrap();
-                    
+
                     // get return and add to commands_to_exec vec
-                    let result: Option<Vec<String>> = func.call(mlua::Variadic::from_iter(args))
+                    let result: Option<Vec<String>> = func
+                        .call(mlua::Variadic::from_iter(args))
                         .expect("[MCRW] Fail to execute callback: {}");
                     if let Some(cmds) = result {
                         commands_to_exec.extend(cmds);
